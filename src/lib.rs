d5d0b00c8bc891c3a2f342f43295bc0b438000d5
Pascal Brandt <psbrandt@gmail.com>
2018-10-06 16:13:26 -0700

    🎬 Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+  Dead = 0,
+  Alive = 1,
+}
