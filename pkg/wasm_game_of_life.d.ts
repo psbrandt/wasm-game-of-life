d22fbfb8e88852b28172ca99c2c6e45173107398
Pascal Brandt <psbrandt@gmail.com>
2018-10-06 19:40:24 -0700

    👨🏻‍🎨 Drawing to canvas.

--- pkg/wasm_game_of_life.d.ts
+++ pkg/wasm_game_of_life.d.ts
@@ -1,3 +1,17 @@
 /* tslint:disable */
-export function greet(arg0: string): void;
+export enum Cell {Dead,Alive,}
+export class Universe {
+free(): void;
+ tick(): void;
 
+static  new(): Universe;
+
+ render(): string;
+
+ width(): number;
+
+ height(): number;
+
+ cells(): number;
+
+}
--- pkg/wasm_game_of_life.js
+++ pkg/wasm_game_of_life.js
@@ -1,6 +1,8 @@
 /* tslint:disable */
 import * as wasm from './wasm_game_of_life_bg';
 
+const __wbg_log_55d0b01f9e295146_target = console.log;
+
 let cachedDecoder = new TextDecoder('utf-8');
 
 let cachegetUint8Memory = null;
@@ -15,34 +17,120 @@ function getStringFromWasm(ptr, len) {
     return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
-export function __wbg_alert_d3b6e8db27c82dfa(arg0, arg1) {
+export function __wbg_log_55d0b01f9e295146(arg0, arg1) {
     let varg0 = getStringFromWasm(arg0, arg1);
-    alert(varg0);
-}
-
-let cachedEncoder = new TextEncoder('utf-8');
-
-function passStringToWasm(arg) {
-    
-    const buf = cachedEncoder.encode(arg);
-    const ptr = wasm.__wbindgen_malloc(buf.length);
-    getUint8Memory().set(buf, ptr);
-    return [ptr, buf.length];
+    __wbg_log_55d0b01f9e295146_target(varg0);
 }
 /**
-* @param {string} arg0
-* @returns {void}
 */
-export function greet(arg0) {
-    const [ptr0, len0] = passStringToWasm(arg0);
-    try {
-        return wasm.greet(ptr0, len0);
-        
-    } finally {
-        wasm.__wbindgen_free(ptr0, len0 * 1);
+export const Cell = Object.freeze({ Dead:0,Alive:1, });
+
+let cachedGlobalArgumentPtr = null;
+function globalArgumentPtr() {
+    if (cachedGlobalArgumentPtr === null) {
+        cachedGlobalArgumentPtr = wasm.__wbindgen_global_argument_ptr();
+    }
+    return cachedGlobalArgumentPtr;
+}
+
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null || cachegetUint32Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory;
+}
+
+const __wbg_random_91b6ba2fda260d29_target = Math.random.bind(Math) || function() {
+    throw new Error(`wasm-bindgen: Math.random.bind(Math) does not exist`);
+};
+
+export function __wbg_random_91b6ba2fda260d29() {
+    return __wbg_random_91b6ba2fda260d29_target();
+}
+
+function freeUniverse(ptr) {
+    
+    wasm.__wbg_universe_free(ptr);
+}
+/**
+*/
+export class Universe {
+    
+    static __construct(ptr) {
+        return new Universe(ptr);
+    }
+    
+    constructor(ptr) {
+        this.ptr = ptr;
         
     }
     
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+        freeUniverse(ptr);
+    }
+    /**
+    * @returns {void}
+    */
+    tick() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        return wasm.universe_tick(this.ptr);
+    }
+    /**
+    * @returns {Universe}
+    */
+    static new() {
+        return Universe.__construct(wasm.universe_new());
+    }
+    /**
+    * @returns {string}
+    */
+    render() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        const retptr = globalArgumentPtr();
+        wasm.universe_render(retptr, this.ptr);
+        const mem = getUint32Memory();
+        const rustptr = mem[retptr / 4];
+        const rustlen = mem[retptr / 4 + 1];
+        
+        const realRet = getStringFromWasm(rustptr, rustlen).slice();
+        wasm.__wbindgen_free(rustptr, rustlen * 1);
+        return realRet;
+        
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        return wasm.universe_width(this.ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        return wasm.universe_height(this.ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    cells() {
+        if (this.ptr === 0) {
+            throw new Error('Attempt to use a moved value');
+        }
+        return wasm.universe_cells(this.ptr);
+    }
 }
 
 export function __wbindgen_throw(ptr, len) {
